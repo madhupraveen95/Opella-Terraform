380489afc8927dd61052eb9868c759fd7c55ebe1
madhupraveen95 <45964337+madhupraveen95@users.noreply.github.com>
2025-11-12 00:44:03 +0530

    Update outputs.tf

--- terraform-azure-vnet/modules/vnet/outputs.tf
+++ terraform-azure-vnet/modules/vnet/outputs.tf
@@ -1,9 +1,9 @@
 output "vnet_id" {
-  description = "The ID of the Virtual Network"
+  description = "vnet id"
   value       = azurerm_virtual_network.this.id
 }
 
 output "subnet_ids" {
-  description = "The IDs of the subnets"
-  value       = { for k, v in azurerm_subnet.subnets : k => v.id }
+  description = "subnet ids"
+  value       = { for s, v in azurerm_subnet.subnets : s => v.id }
 }
